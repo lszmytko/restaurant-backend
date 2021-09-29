16f248813cd64d3c6f1db748e1f3a1f99a103b86
Lukasz Szmytko <lszmytko@gmail.com>
2021-09-29 09:49:39 +0200

    12 commit

--- dbConfig.js
+++ dbConfig.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const { Client } = require("pg");
 
 const client = new Client({
-  user: process.env.DB_USER
+  user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
