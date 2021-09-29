1fb0bf62c8b48227064c07c4684ba5a18e066315
Lukasz Szmytko <lszmytko@gmail.com>
2021-09-29 07:52:08 +0200

    first commit

---
+++ dbConfig.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+
+const { Client } = require("pg");
+
+const client = new Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  ssl: { rejectUnauthorized: false }
+});
+
+client.connect();
+
+module.exports = { client };
