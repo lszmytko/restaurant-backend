dd9a42c84b3e68851fe18938fbb285eca92d03a6
Lukasz Szmytko <lszmytko@gmail.com>
2021-09-29 09:27:44 +0200

    9 commit

--- server.js
+++ server.js
@@ -9,14 +9,7 @@ const cookieParser = require("cookie-parser");
 
 const PORT = 4000;
 
-// app.use(allowCrossDomain)
-app.use(
-  cors({
-    origin: "*",
-    credentials: true, //access-control-allow-credentials:true
-    optionSuccessStatus: 200,
-  })
-);
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
