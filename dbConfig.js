832adeb2a832de671af6ee12a94fdd4665ffcc68
Lukasz Szmytko <lszmytko@gmail.com>
2021-09-29 09:48:56 +0200

    11 commit

--- dbConfig.js
+++ dbConfig.js
@@ -3,11 +3,11 @@ require("dotenv").config();
 const { Client } = require("pg");
 
 const client = new Client({
-  user: 'ebuielxsozitxz',
-  host: 'ec2-54-73-68-39.eu-west-1.compute.amazonaws.com',
-  database: 'd7ntvp15kaa1jh',
-  password: '9898537180f929ba268da6b643a060295123cf0e20edc556840f8d31695995e7',
-  port: 5432,
+  user: process.env.DB_USER
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
   ssl: {
     rejectUnauthorized: false
   }
