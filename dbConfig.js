95ffc944757443b68e774e2dc31a2eb901f3f2ea
Łukasz Szmytko <lukasz.szmytko@grupawp.pl>
2023-01-14 22:45:33 +0100

    changes in dbConfig

--- dbConfig.js
+++ dbConfig.js
@@ -3,11 +3,14 @@ require("dotenv").config();
 const { Client } = require("pg");
 
 const client = new Client({
-  user: "lszmytko",
-  host: "localhost",
-  database: "sklepusers",
-  password: "3Edc4rfv",
-  port: 5432,
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 client
